ec66a43f56c3163eef3d887a70d2b6a0bccdf0f8
OmriHarary <omriharary@hotmail.com>
2023-03-20 16:00:02 -0400

    InstitutionsListener improvements
    - Ignore robots.txt rules on the first page load, agreed was reasonable since this is not part of a crawl and the page would have either been publicly listed or manually identified by us
    - Assume pages with no set locale are in English, since we are targeting primarily English-speaking countries
    - findDepartmentSite was erroneously being invoked with the initial confidence of having found the faculty list instead
    - re-calculate faculty list location confidence after finding department site, since it is a new starting location for the crawl now

--- src/main/java/io/github/aaejo/profilefinder/messaging/consumer/InstitutionsListener.java
+++ src/main/java/io/github/aaejo/profilefinder/messaging/consumer/InstitutionsListener.java
@@ -2,13 +2,14 @@ package io.github.aaejo.profilefinder.messaging.consumer;
 
 import java.util.Locale;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.nodes.Document;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-import io.github.aaejo.messaging.records.Institution;
 import io.github.aaejo.finder.client.FinderClient;
+import io.github.aaejo.messaging.records.Institution;
 import io.github.aaejo.profilefinder.finder.DepartmentFinder;
 import io.github.aaejo.profilefinder.finder.FacultyFinder;
 import io.github.aaejo.profilefinder.finder.ProfileFinder;
@@ -38,16 +39,22 @@ public class InstitutionsListener {
         log.info("Processing {} ({})", institution.name(), institution.country());
         log.debug(institution.toString());
 
-        Document page = client.get(institution.website());
+        // Allow ignoring robots.txt rules on this one, because it's the initial page load.
+        // This will either be the institution home page, or one that has been manually identified for use.
+        Document page = client.get(institution.website(), false);
 
         if (page == null) {
             log.error("Failed to load site for {}", institution.name());
             return;
         }
 
-        // NOTE: Do we assume that no language is english? It's something that happens a lot
         Locale siteLocale = Locale.forLanguageTag(page.getElementsByTag("html").first().attr("lang"));
-        if (!siteLocale.getLanguage().equals(Locale.ENGLISH.getLanguage())) {
+        if (StringUtils.isNotBlank(siteLocale.getLanguage()) // Despite being required, sometimes a locale isn't set
+                                                             // however we are only targeting primarily English-speaking
+                                                             // countries and as such will assume an unset language
+                                                             // is English.
+                && !siteLocale.getLanguage().equals(Locale.ENGLISH.getLanguage())) {
+            // If the language is set and it's not English, we throw and skip this institution
             log.error("Unable to process non-English websites. {} site's language is {}", institution.name(),
                     siteLocale.getDisplayLanguage());
             throw new InstitutionLocaleInvalidException(institution, siteLocale);
@@ -58,7 +65,9 @@ public class InstitutionsListener {
             double foundDepartmentSite = departmentFinder.foundDepartmentSite(page);
             if (foundDepartmentSite < 1) { // Some institutions may already have the department page identified
                 // Find department site
-                page = departmentFinder.findDepartmentSite(institution, page, foundFacultyList);
+                page = departmentFinder.findDepartmentSite(institution, page, foundDepartmentSite);
+                // Re-calculate faculty list confidence because page changed
+                foundFacultyList = facultyFinder.foundFacultyList(page);
             }
             // Find faculty list
             page = facultyFinder.findFacultyList(institution, page, foundFacultyList);
